03200943baceb395b444623c39774b20b341cebc
welkerxa <welkerxa@amazon.com>
2026-04-01 16:21:53 -0700

    added descriptions to all keymaps for which-key

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,27 +2,25 @@
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Toggle NvimTree" })
 
 --fugitive.lua keymaps
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git status (Fugitive)" })
 
 -- harpoon keymaps
 local mark = require "harpoon.mark"
 local ui = require "harpoon.ui"
 local term = require "harpoon.term"
-vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, {})
-vim.keymap.set("n", "<leader>a", mark.add_file, {})
-vim.keymap.set("n", "<C-j>", ui.nav_next, {})
-vim.keymap.set("n", "<C-k>", ui.nav_prev, {})
-vim.keymap.set("n", "<C-t>", function()
-   term.gotoTerminal(1)
-end)
+vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, { desc = "Harpoon menu" })
+vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon add file" })
+vim.keymap.set("n", "<C-j>", ui.nav_next, { desc = "Harpoon next" })
+vim.keymap.set("n", "<C-k>", ui.nav_prev, { desc = "Harpoon prev" })
+vim.keymap.set("n", "<C-t>", function() term.gotoTerminal(1) end, { desc = "Harpoon terminal" })
 
 -- lsp keymaps
-vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
-vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol)
-vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action)
-vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references)
-vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP hover" })
+vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, { desc = "Workspace symbol" })
+vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { desc = "Open diagnostic" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
+vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, { desc = "Code action" })
+vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, { desc = "References" })
+vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { desc = "Rename symbol" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,9 +3,9 @@ return {
    dependencies = { 'nvim-lua/plenary.nvim' },
    cmd = "Telescope",
    keys = {
-      { "<leader>pf", function() require("telescope.builtin").find_files() end },
-      { "<C-g>", function() require("telescope.builtin").git_files() end },
-      { "<leader>ps", function() require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") }) end },
+      { "<leader>pf", function() require("telescope.builtin").find_files() end, desc = "Find files" },
+      { "<C-g>", function() require("telescope.builtin").git_files() end, desc = "Git files" },
+      { "<leader>ps", function() require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") }) end, desc = "Grep string" },
    },
    config = function()
       local actions = require "telescope.actions"
